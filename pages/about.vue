dfece3fb165c166da493663db44b2b5fb1ead112
Yaroslav7125 <bahanovaroslav@gmail.com>
2021-08-02 23:37:50 +0300

    Add page About.

--- pages/about.vue
+++ pages/about.vue
@@ -1,6 +1,13 @@
 <template>
   <section>
+    <h1>About</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati, quasi!</p>
-    
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+      Alias ea earum, fugit in libero maiores nihil obcaecati
+      officiis quaerat quos. Aliquid amet illo impedit laborum omnis
+      quasi quos reprehenderit sequi?</p>
+
+
   </section>
 </template>
