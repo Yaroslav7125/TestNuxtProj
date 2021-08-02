02185a84a6a6b58617f02865e69d60a8a7fd4f15
Yaroslav7125 <bahanovaroslav@gmail.com>
2021-08-03 01:59:14 +0300

    Add no-prefetch for links

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -8,7 +8,7 @@
         <div class="collapse navbar-collapse" id="navbarNavDropdown">
           <ul class="navbar-nav">
             <li class="nav-item ">
-              <nuxt-link exact active-class="active active2" class="nav-link" to="/">Home</nuxt-link>
+              <nuxt-link exact no-prefetch active-class="active active2" class="nav-link" to="/">Home</nuxt-link>
             </li>
             <li class="nav-item">
               <nuxt-link active-class="active active2" class="nav-link" to="about">About</nuxt-link>
